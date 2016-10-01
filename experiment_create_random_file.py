06e666b7a806dff60942ffc9885b353a1f964f3e
Spencer Elliott <me@elliottsj.com>
2016-10-01 17:54:42 -0400

    Create 2^26-byte file in experiment_create_random_file.py

--- experiment_create_random_file.py
+++ experiment_create_random_file.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 
-print('test')
+import subprocess
+
+def create_random_file(filename, total_size, block_size):
+    result = subprocess.run(
+        ['./create_random_file', filename, str(total_size), str(block_size)],
+        stdout=subprocess.PIPE,
+    )
+    return int(result.stdout)
+
+
+def main():
+    elapsed_ms = create_random_file('file.bin', 2 ** 26, 2 ** 14)
+    print('Created file.bin using {} milliseconds'.format(elapsed_ms))
+
+if __name__ == '__main__':
+    main()
