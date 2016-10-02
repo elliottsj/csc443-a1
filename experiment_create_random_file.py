a028d78243b47573120201b9256e32a82bacd5a3
Spencer Elliott <me@elliottsj.com>
2016-10-02 14:04:37 -0400

    Create files in ./out/ with varying block sizes

--- experiment_create_random_file.py
+++ experiment_create_random_file.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
+import os
+import shutil
 import subprocess
 
 def create_random_file(filename, total_size, block_size):
+    """
+    Execute ./create_random_file with the given arguments and return the number of milliseconds
+    it took to create the file.
+    """
     result = subprocess.run(
         ['./create_random_file', filename, str(total_size), str(block_size)],
         stdout=subprocess.PIPE,
@@ -11,8 +17,34 @@ def create_random_file(filename, total_size, block_size):
 
 
 def main():
-    elapsed_ms = create_random_file('file.bin', 2 ** 26, 2 ** 14)
-    print('Created file.bin using {} milliseconds'.format(elapsed_ms))
+    """
+    Call ./create_random_file to create ten 50 MiB files, each using a unique block size
+    ranging from 128 B to 2 MiB
+    """
+    block_sizes = [
+        128,           # 128 B
+        512,           # 512 B
+        1 * 2 ** 10,   # 1 KiB
+        4 * 2 ** 10,   # 4 KiB
+        8 * 2 ** 10,   # 8 KiB
+        64 * 2 ** 10,  # 64 KiB
+        128 * 2 ** 10, # 128 KiB
+        512 * 2 ** 10, # 512 KiB
+        1 * 2 ** 20,   # 1 MiB
+        2 * 2 ** 20,   # 2 MiB
+    ]
+
+    # Write files to ./out/file_{block_size}
+    shutil.rmtree('./out')
+    os.mkdir('./out')
+    for block_size in block_sizes:
+        filename = './out/file_{block_size}.bin'.format(block_size=block_size)
+        elapsed_ms = create_random_file(
+            filename,
+            50 * 2 ** 20,  # 50 MiB
+            block_size,
+        )
+        print('Created {} after {} milliseconds'.format(filename, elapsed_ms))
 
 if __name__ == '__main__':
     main()
